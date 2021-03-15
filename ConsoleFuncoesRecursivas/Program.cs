1a437c4caee5477e1f84e77950eeee1016399da4
Daniel <daniel.meloso@outlook.com>
2021-03-15 15:28:50 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleFuncoesRecursivas/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleFuncoesRecursivas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LerArquivo(1);
+        }
+
+        private static void LerArquivo(int numeroArquivo)
+        {
+            string arquivoComCaminho = (@"C:\TesteArquivos\arq" + numeroArquivo + ".txt");
+            if (File.Exists(arquivoComCaminho))
+            {
+                using (StreamReader arquivo = File.OpenText(arquivoComCaminho))
+                {
+                    string linha;
+                    while ((linha = arquivo.ReadLine()) != null)
+                    {
+                        Console.WriteLine(linha);
+                    }
+                }
+            } else
+            {
+                Console.Write("Arquivo inexistente no diretório especificado");
+            }
+            numeroArquivo++;
+            string proxArquivoComCaminho = (@"C:\TesteArquivos\arq" + ( numeroArquivo ) + ".txt");
+            if (File.Exists(proxArquivoComCaminho))
+            {
+                LerArquivo(numeroArquivo);
+            } else
+            {
+                Console.Write("Fim da leitura dos arquivos...");
+            }
+        }
+    }
+
+    
+}
---
+++ ConsoleFuncoesRecursivas/redme.md
@@ -0,0 +1,6 @@
+Funções recursivas
+
+Ato de uma função chamar a mesma função executando uma ação de forma recursiva.
+Entrando em looping
+
+leitura de arquivos .txt e exibindo o conteúdo no console
